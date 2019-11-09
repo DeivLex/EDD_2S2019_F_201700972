a412ef7bc9548fa5534b413cba95402d78c622dd
DeivLex <peloliso.98@gmail.com>
2019-11-09 16:29:47 -0600

    Avances

---
+++ Final/src/pkgfinal/Final.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author Davis
+ */
+public class Final {
+    public static Arbol arbol = new Arbol();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio v = new Inicio();
+        v.show();
+    }    
+}
